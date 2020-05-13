a1ec86eb9a4278fe797b283a6685677a6363334c
marcusTm <marcusmathiesen@yahoo.dk>
2020-05-13 11:13:36 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
