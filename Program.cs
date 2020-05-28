fdbe96d475fd56415de72ad3c5df28df299cf551
thorsteen <tsl@Thors-PC.localdomain>
2020-05-28 17:35:03 +0200

    formattering af prints

--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@ namespace DataStreams
     {
         static void Main(string[] args)
         {
+            int n = 100000;
+            Console.WriteLine("Test for size n ="+n.ToString());
             for (int i = 1; i < 30; i++)
             {
-                Tests tests = new Tests(1000000,i);
+                Console.WriteLine("Size l: "+ i.ToString());
+                Tests tests = new Tests(n,i);
                 //Console.WriteLine("Hash function tests");
                 //tests.TestMultiplyShiftHashingFunction();
                 //tests.TestMultiplyModPrimeHashingFunction();
