76f449d10e860b441fc28b83960a33c428a26777
thorsteen <tsl@Thors-PC.localdomain>
2020-06-03 12:56:28 +0200

    har lavet plots til rapport

--- Ex_results/plots.py
+++ Ex_results/plots.py
@@ -1,54 +1,44 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-mean_rho_data = np.loadtxt("eval_mean_rho.txt", dtype = str, delimiter = "\n")
-#we know there shoould be 12 values in both
-mean_rho_SGNS = []
-mean_rho_CBOW = []
-model_name_SGNS = []
-model_name_CBOW = []
+ex7_arr = np.genfromtxt('Count-sketch_sorted_estimates.csv',delimiter=',',dtype = float)
+ex8_arr3 = np.genfromtxt('Count-sketch_sorted_estimates_t3.csv',delimiter=',',dtype = float)
+ex8_arr5 = np.genfromtxt('Count-sketch_sorted_estimates_t5.csv',delimiter=',',dtype = float)
+ex8_arr10 = np.genfromtxt('Count-sketch_sorted_estimates_t10.csv',delimiter=',',dtype = float)
 
-for i, data in enumerate(mean_rho_data):
-    if i == len(mean_rho_data)-1:
-        print("made arrays of values")
-    else:
-        if "cbow" in str(data):
-            model_name_CBOW.append(mean_rho_data[i])
-            mean_rho_CBOW.append(float(mean_rho_data[i+1]))
-        if "skipgram" in str(data):
-            model_name_SGNS.append(mean_rho_data[i])
-            mean_rho_SGNS.append(float(mean_rho_data[i+1]))
 
-mean_rho_SGNS = np.array(mean_rho_SGNS)
-mean_rho_CBOW = np.array(mean_rho_CBOW)
-#mean_rho_SGNS.sort() #we fuck it up if we sort
-#mean_rho_CBOW.sort() #could make tuples instead...
+ex7_plot, ex7_ax = plt.subplots()
+ex7_ax.spines["top"].set_visible(False)
+ex7_ax.spines["right"].set_visible(False)
+ex7_ax.spines["bottom"].set_visible(False)
+ex7_ax.spines["left"].set_visible(False)
+ex7_ax.plot(ex7_arr[:,0], ex7_arr[:,1], color='black',label = 'X')
+ex7_ax.plot([0,100], [137296,137296], color='grey',label='S')
+ex7_ax.legend(loc='upper left', frameon=False)
+plt.xticks(np.arange(start=1, stop=101, step=9))
+plt.ylim(np.amin(ex7_arr[:,1])-1000, np.amax(ex7_arr[:,1])+1000)
+plt.xlim(1, len(ex7_arr[:,0]))
+ex7_ax.set_title("Count-Sketch using t = 10 on a data stream with l = 13 and n = 100.000")
+plt.ylabel("X and S sums")
+plt.xlabel("X_1 ... X_{}".format(len(ex7_arr)))
+ex7_plot.set_size_inches(11.69,4)
+ex7_plot.savefig("ex7_plot.png")
 
-print("skip-gram evaluation WS-353 mean values, windows size 1 - {}".format(len(mean_rho_SGNS)))
-
-#first results
-#mean_rho_SGNS = [0.6356543304967629,0.6859671484411557  ,0.6924293781318873  , 0.7077708305390136,0.7188599859909027 , 0.7132944845703011 , 0.699551202038216, 0.7192855118852405,  0.7041803792159528,  0.717889605224073]
-#window_size_SGNS =np.arange(1,len(mean_rho_SGNS)+1,1)
-
-for i in range(len(mean_rho_SGNS)):
-    print(model_name_SGNS[i] + ": " + str(mean_rho_SGNS[i]))
-
-SGNS_plot, ax_SGNS = plt.subplots()
-ax_SGNS.plot(model_name_SGNS, mean_rho_SGNS)
-plt.xticks(np.arange(len(model_name_SGNS)), model_name_SGNS, rotation=20)
-ax_SGNS.set_title("skip-gram evaluation WS-353 mean rho values, windows size 1 - {}".format(len(mean_rho_SGNS)))
-SGNS_plot.savefig("SGNS_plot.png")
-
-#first results
-#mean_rho_CBOW = [0.5804338343545967 , 0.6508858227099523 ,0.6802805346625579  ,0.6932546677573747  , 0.7024608832285866, 0.7090981595837526 ,0.7075484759236735, 0.722764153135901, 0.7121865820323352, 0.7234083268868999 ]
-#window_size_CBOW = np.arange(1,len(mean_rho_CBOW)+1,1)
-
-print("CBOW evaluation on WS-353 mean rho values, windows size 1 - {}".format(len(mean_rho_CBOW)))
-for i in range(len(mean_rho_CBOW)):
-    print(model_name_CBOW[i] + ": " + str(mean_rho_CBOW[i]))
-
-CBOW_plot, ax_CBOW = plt.subplots()
-ax_CBOW.plot(model_name_CBOW, mean_rho_CBOW)
-plt.xticks(np.arange(len(model_name_CBOW)), model_name_CBOW, rotation=20)
-ax_CBOW.set_title("CBOW evaluation WS-353 mean values, windows size 1 - {}".format(len(mean_rho_CBOW)))
-CBOW_plot.savefig("CBOW_plot.png")
+ex8_plot, ex8_ax = plt.subplots()
+ex8_ax.spines["top"].set_visible(False)
+ex8_ax.spines["right"].set_visible(False)
+ex8_ax.spines["bottom"].set_visible(False)
+ex8_ax.spines["left"].set_visible(False)
+ex8_ax.plot(ex8_arr3[:,0], ex8_arr3[:,1], color='blue', label='X with t=3')
+ex8_ax.plot(ex8_arr5[:,0], ex8_arr5[:,1], color='yellow',label='X with t=5')
+ex8_ax.plot(ex8_arr10[:,0], ex8_arr10[:,1], color='red',label='X with t=10')
+ex8_ax.plot([0,100], [137296,137296], color='grey',label='S')
+ex8_ax.legend(loc='upper left', frameon=False)
+plt.xticks(np.arange(start=1, stop=101, step=9))
+plt.ylim(np.amin(ex7_arr[:,1])-1000, np.amax(ex7_arr[:,1])+1000)
+plt.xlim(1, len(ex7_arr[:,0]))
+ex8_ax.set_title("Count-Sketch using t = 10 on a data stream with l = 13 and n = 100.000")
+plt.ylabel("X and S sums")
+plt.xlabel("X_1 ... X_{}".format(len(ex7_arr)))
+ex8_plot.set_size_inches(11.69,4)
+ex8_plot.savefig("ex8_plot.png")
